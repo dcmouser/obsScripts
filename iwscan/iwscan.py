29ab3eb58660ab22da39ec391dde21b9ba9862aa
dc mouser <mouser@donationcoder.com>
2022-07-28 13:55:49 -0500

    updates
    just some minor updates

--- iwscan/iwscan.py
+++ iwscan/iwscan.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 """
+v2.3 - 7/6/22 - mouser@donationcoder.com
 this tool scans infoWriter text output and writes all events that happen after a break or on a hotkey press
 it uses the FIRST hotkey it finds before a break to set the starting offset of the stream
 OR you can manually set the starting stream offset time by simply adding a HOTKEY:HOTKEY GOLIVE entry with the real Stream Time Marker time
+ALSO you can modify whatever the calculated timestamps are by adding a line: "OFFSET 00:00:01" or "OFFSET -00:00:01" to add or subtract a second
+
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Usage: iwscan <src>
 """
@@ -49,11 +52,26 @@ def doProcessFile(fileIn, fileOut, timeStampKeyword, heuristicResetAtFirstHotkey
     breakCount = 0
     writeLines = 0
     # regex compiles
-    regexEventBreak = re.compile("^EVENT\:(.*Title.*)")
+#    regexEventStartingSoon = re.compile("^EVENT\:(.*Starting Soon.*)")
+    regexEventStartingSoon = re.compile(".*Starting.*")
+    regexEventBreak = re.compile(".*EVENT\:(.*Title.*)")
+    regexEventBreak2 = re.compile("^EVENT\:(.*Intro Video.*)")
     regexEventNonBreak = re.compile("^EVENT\:(.*)")
     regexGoLive = re.compile("(.*GOLIVE.*)")
     regexHotkey = re.compile("^HOTKEY\:(.*)")
     regexStreamTime = re.compile("^(.*) " + timeStampKeyword + " Time Marker$")
+    regexEventStartStreaming = re.compile("^EVENT\:START STREAM \@ [0-9\-]+ (.*)$")
+    regexEventStartBroadcasting = re.compile("^EVENT\:START BROADCASTING \@ [0-9\-]+ (.*)$")
+    regexOffset = re.compile("^OFFSET (.*)$")
+    # this will be assumed as the time that the youtube stream went live
+    assumedStreamingStartTimestamp = "23:00:00"
+    startingClockTimeStamp = ""
+    assumedStartRecordingOffset = 0
+    adjustOffsetTimestampSecs = 0
+    guessingStarTime = True
+    #
+    #DefHotkeyStartStreamTimeCutoff = 0
+    DefHotkeyStartStreamTimeCutoff = 60 * 20
 
     # Process the file line-by-line
     with open(fileIn, 'r') as fpIn, open(fileOut, 'w') as fpOut:
@@ -61,23 +79,56 @@ def doProcessFile(fileIn, fileOut, timeStampKeyword, heuristicResetAtFirstHotkey
         for line in fpIn:
             lineNumber += 1
             line = line.rstrip()   # Strip trailing spaces and newline
+            #print(line)
             # see what kind of line it is
-            if (regexEventBreak.match(line)):
+            if (regexOffset.match(line)):
+                # start of stream, get real world time
+                result = regexOffset.search(line)
+                offsetTimestamp = result.group(1)
+                # manual adjustment
+                adjustOffsetTimestampSecs = convertTimeStampToSeconds(offsetTimestamp)
+
+            elif (regexEventStartBroadcasting.match(line)):
+            	  # NEW EVENT THAT REALLY MARKS START OF GOING LIVE
+                # is this all we need
+                withNextTimestamp = "streamStartTimeReset"
+                # tweak
+                adjustOffsetTimestampSecs = -1
+                # no more guessing
+                guessingStarTime = False
+
+
+            elif (regexEventStartStreaming.match(line) and (assumedStreamingStartTimestamp != "")):
+                # start of stream, get real world time
+                result = regexEventStartStreaming.search(line)
+                startingClockTimeStamp = result.group(1)
+                # the offset is the assumed stream broadcast wall clock start time - stream wall clock record start time
+                assumedStartRecordingOffset = convertTimeStampToSeconds(assumedStreamingStartTimestamp) - convertTimeStampToSeconds(startingClockTimeStamp)
+            elif ((regexEventBreak.match(line)) or (regexEventBreak2.match(line))):
                 # event break
                 inBreakFlag = True
                 currentEventLine = line
                 withNextTimestamp = ""
                 breakCount = breakCount + 1
+                # if this break is a starting soon, it MAY indicate that youtube stream has started
+                if (regexEventStartingSoon.match(line)):
+                    # starting soon display (the LAST ONE) may indicate right before we go live, if user switched to it right before pressing GO LIVE on youtube
+                    if guessingStarTime:
+                        withNextTimestamp = "streamStartTimeReset"
             elif (regexGoLive.match(line)):
                 # go live marker
                 withNextTimestamp = "streamStartTimeReset"
+                guessingStarTime = False
             elif (regexHotkey.match(line)):
                 # go hotkey
                 currentEventLine = line
                 withNextTimestamp = "write"
-                if (writeLines == 0) and (timeOffsetSeconds == 0) and (heuristicResetAtFirstHotkey):
+                writeRawLineToFile(fpOut, "// Found hotkey 1")
+                if (writeLines == 0) and (timeOffsetSeconds <= DefHotkeyStartStreamTimeCutoff) and (heuristicResetAtFirstHotkey):
                     # kludge, if first hotkey comes before first break, we use it to reset stream time
                     withNextTimestamp = "writeAndStreamStartTimeReset"
+                    guessingStarTime = False
+                    writeRawLineToFile(fpOut, "// Found hotkey 2")
             elif (regexStreamTime.match(line)):
                 # should we write it or do something with it
                 result = regexStreamTime.search(line)
@@ -89,9 +140,15 @@ def doProcessFile(fileIn, fileOut, timeStampKeyword, heuristicResetAtFirstHotkey
                     # write it out
                     if (writeLines == 0):
                         # initial start time
+                        writeRawLineToFile(fpOut, "// TIMESTAMP OUTPUTS")
+                        writeRawLineToFile(fpOut, "// To manually add offset use: OFFSET 0:00:00")
                         writeTimestampLineToFile(fpOut, "0:00:00", "Start of stream")
+                    # if no time offset is found, we can try to guess it
+                    if (timeOffsetSeconds==0):
+                        # no time offset so can we guess it using assumedStreamingStartTimestamp
+                        timeOffsetSeconds = assumedStartRecordingOffset
                     # write it
-                    timeStampTextAdjusted = adjustTimeStamp(timeStampText,timeOffsetSeconds)
+                    timeStampTextAdjusted = adjustTimeStamp(timeStampText,timeOffsetSeconds - adjustOffsetTimestampSecs)
                     writeTimestampLineToFile(fpOut, timeStampTextAdjusted, currentEventLine)
                     writeLines = writeLines + 1                
                 # clear
@@ -116,8 +173,15 @@ def adjustTimeStamp(timeStampText, timeOffsetSeconds):
 
 def convertTimeStampToSeconds(timeStampText):
     # see https://stackoverflow.com/questions/6402812/how-to-convert-an-hmmss-time-string-to-seconds-in-python
+    if (timeStampText==""):
+    	return 0
+    sgn = 1
+    if (timeStampText[0]=='-'):
+    	sgn = -1
+    	timeStampText = timeStampText[1:]
     h, m, s = timeStampText.split(':')
-    return int(h) * 3600 + int(m) * 60 + int(s)
+    retv = sgn * (int(h) * 3600 + int(m) * 60 + int(s))
+    return retv
 
 
 def converSecondsToTimeStamp(secs):
@@ -130,6 +194,10 @@ def writeTimestampLineToFile(fpOut, timeStampText, lineLabel):
     timeStampTextClean = cleanTimeStampForOutput(timeStampText)
     fpOut.write("{} - {}\n".format(timeStampTextClean, lineLabel))
     # Need \n, which will be translated to platform-dependent newline
+
+
+def writeRawLineToFile(fpOut, str):
+    fpOut.write("{}\n".format(str))
 
 
 def cleanTimeStampForOutput(timeStampText):
